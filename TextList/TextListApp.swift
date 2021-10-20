dea8a817a859417084f04b5fd0a4ee6abf27328f
Alex <alexrozh1988@gmail.com>
2021-10-20 12:48:27 +0300

    Initial Commit

---
+++ TextList/TextListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextListApp.swift
+//  TextList
+//
+//  Created by Alexander Rozhdestvenskiy on 20.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct TextListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
